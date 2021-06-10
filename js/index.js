027302329dc1d140b510e8157b1098149e8f300d
liqiujiong <pelle@live.cn>
2021-06-10 19:39:19 +0800

    界面

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            flag: false, //单条or多条
+            unclick: false, //重复点击
+            resdata: [], //数据结果
+            
+            errtip: "", //错误提示
+            count:0,
+            content:1000,
+
+        },
+        methods: {
+
+        },
+    })
+
+    
+}
