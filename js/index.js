7c1dd54f35d056070d5f297d003ab0bbc41925e5
liqiujiong <pelle@live.cn>
2021-06-10 22:02:01 +0800

    界面调整、增、删、完成todo

--- js/index.js
+++ js/index.js
@@ -3,17 +3,99 @@ window.onload = function () {
     var vm = new Vue({
         el: '#app',
         data: {
-            flag: false, //单条or多条
-            unclick: false, //重复点击
-            resdata: [], //数据结果
-            
-            errtip: "", //错误提示
-            count:0,
-            content:1000,
+            inputText:'',
+            searchList:[],
+            list:[],
+            item:{
+                id:null,
+                content:null,
+                isFinish:false,
+            }
+        },
+        created() {
+            localStorage.setItem('data',{name:'lqj'})
+            this.list = [
+                {
+                    id:21,
+                    content:'测试flase',
+                    isFinish:false
+                },
+                {
+                    id:53,
+                    content:'测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长',
+                    isFinish:false
+                },
+                {
+                    id:22,
+                    content:'测试true',
+                    isFinish:true
+                },
+                {
+                    id:23,
+                    content:'测试flase',
+                    isFinish:false
+                },
+                {
+                    id:24,
+                    content:'测试true',
+                    isFinish:true
+                },
+            ]
+            console.log('初始list:'+this.list)
+
+        },
+        mounted() {
+
+        },
+        computed: {
 
         },
         methods: {
+            //搜索事件绑定
+            //搜索
+            search(){
 
+            },
+            //添加一条待办
+            addItem(){
+                if(this.inputText == ''){
+                    alert('请输入待办内容')
+                    return
+                }
+                this.list.push({
+                    id:( new Date()).valueOf(),
+                    content:this.inputText,
+                    isFinish:false,
+                }),
+                this.inputText = '',
+                console.log(this.list)
+                alert('添加成功')
+            },
+            //删除一条待办
+            delItem(item){
+                console.log('del item:',item)
+                for (var i = 0; i < this.list.length; i++) {
+                    if (this.list[i].id == item.id) {
+                        break;
+                    }
+                }
+                if(i != this.length){
+                    this.list.splice(i, 1);
+                }
+            },
+            //更改待办状态
+            updateItem(item){
+                console.log(item)
+            },
+            //编辑content
+            editItemContent(item){
+                
+            },
+            //更新localStorage
+            updateLocalStorage(){
+
+            }
+            
         },
     })
 
