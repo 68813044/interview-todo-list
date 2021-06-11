16ffa423c886bbc39f53a02f1be991daa78491e1
liqiujiong <pelle@live.cn>
2021-06-11 12:01:58 +0800

    完善搜索 编辑

--- js/index.js
+++ js/index.js
@@ -1,60 +1,64 @@
 window.onload = function () {
+    var listStorage = {
+        fetch: function() {
+          var list = JSON.parse(localStorage.getItem("todoList") || "[]");
+          console.log("get localStorage:",list)
+          return list;
+        },
+        save: function(list) {
+          localStorage.setItem("todoList", JSON.stringify(list));
+        }
+      };
 
     var vm = new Vue({
         el: '#app',
         data: {
             inputText:'',
-            searchList:[],
-            list:[],
-            item:{
-                id:null,
-                content:null,
-                isFinish:false,
+            editing:false,
+            list:listStorage.fetch(),
+            showOption:'all',
+            editItem:null
+        },
+        computed:{
+            filteredList: function() {
+                if(this.showOption == 'all'){
+                    return this.search(this.list);
+                }else if(this.showOption == 'finish'){
+                    return this.search(this.list.filter(function(item) {
+                        return item.isFinish;
+                    }));
+                }else{
+                    return this.search(this.list.filter(function(item) {
+                        return !item.isFinish;
+                    }));
+                }
+            },
+        },
+        directives: {
+            "item-focus": function(el, binding) {
+                if (binding.value) {
+                el.focus();
+                }
             }
         },
-        created() {
-            localStorage.setItem('data',{name:'lqj'})
-            this.list = [
-                {
-                    id:21,
-                    content:'测试flase',
-                    isFinish:false
+        watch: {
+            list: {
+                handler: function(list) {
+                    console.log("watch save localStorage:",list)
+                    listStorage.save(list);
                 },
-                {
-                    id:53,
-                    content:'测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长测试flase长',
-                    isFinish:false
-                },
-                {
-                    id:22,
-                    content:'测试true',
-                    isFinish:true
-                },
-                {
-                    id:23,
-                    content:'测试flase',
-                    isFinish:false
-                },
-                {
-                    id:24,
-                    content:'测试true',
-                    isFinish:true
-                },
-            ]
-            console.log('初始list:'+this.list)
-
-        },
-        mounted() {
-
-        },
-        computed: {
-
+                deep: true
+            },
         },
         methods: {
-            //搜索事件绑定
             //搜索
-            search(){
-
+            search(data){
+                if(this.inputText == ''){
+                    return data
+                }
+                return data.filter(item => {
+                    return item.content.indexOf(this.inputText) != -1;
+                });
             },
             //添加一条待办
             addItem(){
@@ -67,9 +71,7 @@ window.onload = function () {
                     content:this.inputText,
                     isFinish:false,
                 }),
-                this.inputText = '',
-                console.log(this.list)
-                alert('添加成功')
+                this.inputText = ''
             },
             //删除一条待办
             delItem(item){
@@ -83,19 +85,27 @@ window.onload = function () {
                     this.list.splice(i, 1);
                 }
             },
-            //更改待办状态
-            updateItem(item){
-                console.log(item)
-            },
-            //编辑content
-            editItemContent(item){
-                
-            },
-            //更新localStorage
-            updateLocalStorage(){
 
+            changeOption(data){
+                this.showOption = data
+            },
+
+
+            //双击编辑
+            toEdit(item){
+                this.editCache = item.content;
+                this.editItem = item;
+            },
+            doneEdit(item){
+                if (!this.editItem) {
+                    return;
+                }
+                this.editItem = null;
+                item.content = item.content.trim();
+                if (!item.content) {
+                    this.delItem(item);
+                }
             }
-            
         },
     })
 
